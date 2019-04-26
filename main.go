9b370f6c5cc0ca1b04d4bcc1d40df4874fcfd01c
Dmitry <avhimkov@mail.ru>
2019-04-26 10:19:05 +0500

    add config.json add func maxima api

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Login    string
+	Password string
+}
+
 func main() {
+	file, _ := os.Open("config.json")
+	decoder := json.NewDecoder(file)
+	configuration := Config{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	configuration.Login
+	configuration.Password
+
+	if err != nil {
+		log.Panic(err)
+	}
+
 	// sms
 	tele2("79827468271")
 	// push
-	SendGMToClient("Hello from GCM", "<CLIENT TOKEN>")
+
+	/* SendGMToClient("Hello from GCM", "<CLIENT TOKEN>") */
 }
