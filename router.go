fb257a663392f8a68b326e226d28f6b27bf3a16e
Dmitry <avhimkov@mail.ru>
2019-07-04 21:50:43 +0500

    add services from web interface

--- router.go
+++ router.go
@@ -74,8 +74,14 @@ func indexPageGet(c *gin.Context) {
 
 func indexPagePost(c *gin.Context) {
 
+	// serviceid := c.PostForm("serviceid")
+	// fio := c.PostForm("fio")
+	// comment := c.PostForm("comment")
+
 	// tiketList := getTicketSteps("3")
-	tiketList := Register("288", "Vany2", "", "0", "")
+
+	// tiketList := Register(serviceid, fio, comment, "0", "")
+	tiketList := Register("288", "Vany3", "", "0", "")
 
 	// {"Command":"cmd_GetTicketSteps","TicketSteps":[{"TicketStepID":"49916","TicketNo":"77","CustID":"49449","CustData":"Ширкина А.П.",
 	// "SourceKind":"1","State":"0","ServiceID":"190","RegTime":"08.05.2019 14:14:40","CallTime":"01.01.2000","PriorityID":"0","QualityMark":"0"}],"ResultCode":"0"}
